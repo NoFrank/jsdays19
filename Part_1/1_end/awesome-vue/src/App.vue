adc01d53eff3181d9abdb3467fbd2693f806739b
Norbert Frank <norbert@nofrank.net>
2019-10-08 15:57:14 +0200

    Part 1

---
+++ Part_1/1_end/awesome-vue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Home :entries="entries"></Home>
+  </div>
+</template>
+
+<script>
+import Home from "./views/Home.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      entries: [
+        {
+          id: 1,
+          title: "Erster Eintrag",
+          description: "Beschreibung Eintrag 1"
+        },
+        {
+          id: 2,
+          title: "Zweiter Eintrag",
+          description: "Beschreibung Eintrag 2"
+        }
+      ]
+    };
+  },
+  components: {
+    Home
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ Part_1/1_end/awesome-vue/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header
+      class="border-b border-color-gray-500 p-4 flex flex-row items-center"
+    >
+      <img src="../assets/logo.png" class="align-center h-8 mr-4" />
+      <h1 class="text-vue text-3xl">Awesome Vue.js</h1>
+    </header>
+    <section></section>
+    <main class="m-4">
+      <h2 class="text-vue text-2xl font-bold">Einträge</h2>
+      <div v-for="entry in entries" :key="entry.id">
+        <div class="font-bold">
+          {{ entry.title }}
+        </div>
+        <div class="mb-2">
+          {{ entry.description }}
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    entries: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
